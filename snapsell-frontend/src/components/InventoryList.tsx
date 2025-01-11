4e789b81388d7223ab92c964173d67de9f57ec71
Collin Qian <collinqian@umail.ucsb.edu>
2025-01-11 11:50:02 -0800

    Image Storage Loading

--- snapsell-frontend/src/components/InventoryList.tsx
+++ snapsell-frontend/src/components/InventoryList.tsx
@@ -7,7 +7,7 @@ import { ItemStatusCard } from './ItemStatusCard'
 const mockItems = [
   { 
     id: 1, 
-    image: '/placeholder.svg', 
+    image: 'https://imgvrvsheaucvxnlqmqz.supabase.co/storage/v1/object/public/item-images/u2548699992_A_boy_in_Indonesia_stands_with_a_confident_look_p_c42b35ff-0382-45ea-9651-4063f1933a75_0.png?t=2025-01-11T19%3A49%3A09.127Z', 
     title: 'Vintage Chair',
     description: 'Beautiful vintage chair in great condition', 
     price: 50, 
